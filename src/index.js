9bb84b55f888b3ef8de75db39b565faa71b93855
akashmukherjee009 <105306932+akashmukherjee009@users.noreply.github.com>
2022-10-25 01:15:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+function sum(input){
+             
+ if (toString.call(input) !== "[object Array]")
+    return false;
+      
+            var total =  0;
+            for(var i=0;i<input.length;i++)
+              {                  
+                if(isNaN(input[i])){
+                continue;
+                 }
+                  total += Number(input[i]);
+               }
+             return total;
+            }
