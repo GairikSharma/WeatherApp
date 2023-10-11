9f2ffbb172184abe4f94ec2fbf84e5325cd9148c
ANIRUDHA PRADHAN <114860976+Anirudha0702@users.noreply.github.com>
2023-10-11 05:11:11 +0000

    display weather details of currentlocation on load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import Card from './components/card';
@@ -22,16 +22,54 @@ function App() {
     pressure: '',
     country: "",
   })
+  const getWeatherByCordinates=async()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async(position) => {
+          console.log(position.coords.latitude)
+            const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=da9d79f0304b0a345bbfa7b59e0c5fd5`)
+            const data=await res.json()
+            console.log(data)
+            const date=new Date(data.dt*1000)
+            setData({
+              description: data.weather[0].description,
+              temp: (data.main.temp).toFixed(1) + "°C",
+              temp_min: (data.main.temp_min).toFixed(1) + "°C",
+              temp_max: ((data.main.temp_max)).toFixed(1) + "°C",
+              pressure: `${data.main.pressure} hPa`,
+              humidity: (data.main.humidity)+"%",
+              wind: (data.wind.speed)+" "+"km/h",
+              sunrise: data.sys.sunrise,
+              sunset: data.sys.sunset,
+              country: data.sys.country,
+              city: `${val}`,
+              icon: data.weather[0].icon,
+              date: `${date.toLocaleDateString('en-US', { weekday: 'long' })}, ${date.toLocaleDateString('en-US', { month: 'long' })}  ${date.getDate()} ${date.getFullYear()}`
+            })
+        },
+        (error) => {
+          window.alert("Cannot fetch your location Please enter city name")
+        }
+    );
+  }
+  else {
+    window.alert("geo Location is not supported by browser")
+  }
+  }
+  useEffect(()=>{
+    getWeatherByCordinates();
+  }
+    ,[])
   const handleClick = () => {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=da9d79f0304b0a345bbfa7b59e0c5fd5`)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${val}&units=metric&appid=da9d79f0304b0a345bbfa7b59e0c5fd5`)
       .then((res) => {
         console.log(res.data);
         setData({
           description: res.data.weather[0].description,
-          temp: ((res.data.main.temp) - 273.15).toFixed(1) + "°C",
-          temp_max: ((res.data.main.temp_max) - 273.15).toFixed(1) + "°C",
-          temp_min: ((res.data.main.temp_min) - 273.15).toFixed(1) + "°C",
-          pressure: (res.data.main.pressure)+" "+"hPa",
+          temp: ((res.data.main.temp) ).toFixed(1) + "°C",
+          temp_min: ((res.data.main.temp_min)).toFixed(1) + "°C",
+          temp_max: ((res.data.main.temp_max)).toFixed(1) + "°C",
+          pressure: `${res.data.main.pressure} hPa`,
           humidity: (res.data.main.humidity)+"%",
           wind: (res.data.wind.speed)+" "+"km/h",
           sunrise: res.data.sys.sunrise,
