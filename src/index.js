c85d356814e9c2303781f16688433376861ced3a
nisoojadhav <nisoojadhav@gmail.com>
2023-10-17 22:46:44 +0530

    grammatical errors resolved, favicon, files formatting using Prettier, removed unnecessary code that came with CRA

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-function sum(input){
-             
- if (toString.call(input) !== "[object Array]")
-    return false;
-      
-            var total =  0;
-            for(var i=0;i<input.length;i++)
-              {                  
-                if(isNaN(input[i])){
-                continue;
-                 }
-                  total += Number(input[i]);
-               }
-             return total;
-            }
+function sum(input) {
+  if (toString.call(input) !== "[object Array]") return false;
+  var total = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
+    }
+    total += Number(input[i]);
+  }
+  return total;
+}
